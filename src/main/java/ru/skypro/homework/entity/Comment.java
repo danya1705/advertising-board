2b4018f6dabbcac9695cc3a2071938cd619a4c99
Guru <viktor.ihtiyarov@gmail.com>
2023-04-26 21:36:54 +0700

    Добавлены конструкторы, геттеры и сеттеры

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -20,4 +20,51 @@ public class Comment {
 
     private String text;
 
+    public Comment(Integer adId, Integer commentId, Integer autor, Long createdAt, String text) {
+        this.adId = adId;
+        this.commentId = commentId;
+        this.autor = autor;
+        this.createdAt = createdAt;
+        this.text = text;
+    }
+
+    public Integer getAdId() {
+        return adId;
+    }
+
+    public void setAdId(Integer adId) {
+        this.adId = adId;
+    }
+
+    public Integer getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Integer commentId) {
+        this.commentId = commentId;
+    }
+
+    public Integer getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Integer autor) {
+        this.autor = autor;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- src/main/java/ru/skypro/homework/entity/UserAds.java
+++ src/main/java/ru/skypro/homework/entity/UserAds.java
@@ -28,4 +28,87 @@ public class UserAds {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public UserAds(Integer id, String userName, String password, String firstName, String lastName, String phone, String email, String image, Role role) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.image = image;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
